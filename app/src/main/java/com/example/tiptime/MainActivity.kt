37d59bc8fd745992f33e4d649353d0d1cbd7e3c0
Souvik Ghosh <souvikghosh.education@gmail.com>
2023-04-05 14:36:32 +0530

    modify EditNumberField function in MainActivity.kt file

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,26 +38,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TipTimeScreen(){
-    Column(modifier = Modifier.padding(32.dp),
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(text = stringResource(id = R.string.calculate_tip),
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
-            )
+        )
         Spacer(modifier = Modifier.height(16.dp))
         EditNumberField()
-
 
 
     }
 
 }
+
 @Composable
-fun EditNumberField(){
-    TextField(value = "",
-        onValueChange ={}
+fun EditNumberField() {
+    var inputAmount by remember { mutableStateOf("") }
+    TextField(
+        value = inputAmount,
+        onValueChange = { inputAmount = it },
+        label = { Text(text = stringResource(id = R.string.cost_of_service)) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+
     )
 
 }

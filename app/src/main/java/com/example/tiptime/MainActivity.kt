89499a078a10b0c175fe6f77e8b988b268f3d6fc
Souvik Ghosh <souvikghosh.education@gmail.com>
2023-04-07 20:17:31 +0530

    set action buttons

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,12 +62,18 @@ fun TipTimeScreen() {
         EditNumberField(
             label = R.string.bill_amount,
             value = inputAmount,
-            onValueChange = { inputAmount = it }
+            onValueChange = { inputAmount = it } ,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next)
         )
         EditNumberField(
             label = R.string.tip_percent,
             value = inputTip,
-            onValueChange = { inputTip = it }
+            onValueChange = { inputTip = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done)
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
@@ -88,6 +95,7 @@ fun EditNumberField(
     @StringRes label: Int,
     value: String,
     onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
     modifier: Modifier = Modifier
 
 
@@ -100,7 +108,7 @@ fun EditNumberField(
         label = { Text(text = stringResource(label)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions
 
 
     )
